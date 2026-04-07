b6268e83f1399f0eafe0b5812d33b05204736cb6
NesmaElsafty <nesmaelsaftysm@gmail.com>
2026-04-07 11:50:31 +0200

    last update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 class Category extends Model implements HasMedia
